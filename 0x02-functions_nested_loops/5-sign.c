badea972e25604b931327decbb189cca413da99f
ACNjogo <acnjogo@gmail.com>
2022-03-15 15:43:33 +0300

    many tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_sign - main
+ *@i: defined number
+ *Return: 1 if positive, 0 if 0m -1 if negative
+ */
+int print_sign(int i)
+{
+if (i > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (i == 0)
+{
+_putchar('0');
+}
+else
+{_putchar('-');
+return (-1);
+}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - main function
+ * @i: integer to make absolute
+ * Return: int or int times negative
+ */
+
+int _abs(int i)
+{
+if (i > 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
