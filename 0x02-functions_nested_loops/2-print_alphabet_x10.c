a7f2f63b0621ececfc41d7329ad829915825ee72
ACNjogo <acnjogo@gmail.com>
2022-03-15 14:56:15 +0300

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
  *Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
 char j;
- for (j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
