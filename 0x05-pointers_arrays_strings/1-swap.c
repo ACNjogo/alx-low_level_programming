466c1a5a155e11f26c8ea5d41c95bee6dc50c8c8
ACNjogo <acnjogo@gmail.com>
2022-03-21 17:24:54 +0300

    fix task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,12 @@
 
 /**
  *swap_int - function that swaps values
- *@a - first value
- *@b - second value
+ *
+ *@a: first value
+ *@b: second value
+ *
  */
-void swap_int(int *a, int*b)
+void swap_int(int *a, int *b)
 {
 int i = *a;
 int j = *b;
