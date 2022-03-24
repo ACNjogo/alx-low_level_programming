cbfb80c43f7b12891da3f5cea8cc72a04b0d4267
ACNjogo <acnjogo@gmail.com>
2022-03-24 11:21:21 +0300

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcmp - function that compares 2 strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int n = 0;
+while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+i++;
+if ((*s2 + i))
+return (*(s1 + i) -*(s2 + i));
+else
+return (0);
+}
